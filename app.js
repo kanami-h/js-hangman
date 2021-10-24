77c551b24f6d9a9ed44bcd0c8c1a2602a7b0e94e
Kanami <kanami.h217@gmail.com>
2021-10-23 22:41:16 -0400

    added: error hundling

--- app.js
+++ app.js
@@ -6,14 +6,30 @@ const remainingNumber = document.querySelector(".remaining-num");
 
 // initial settings
 window.addEventListener("DOMContentLoaded", () => {
-  puzzle.textContent = game.puzzle; // getPuzzle();
-  message.innerHTML = game.statusMessage; // showMessage();
+  puzzle.textContent = game.puzzle;
+  message.innerHTML = game.statusMessage;
 });
 
-window.addEventListener("keypress", function (e) {
+window.addEventListener("keypress", (e) => {
   const guess = String.fromCharCode(e.charCode);
   game.makeGuess(guess);
   // display the puzzle
-  puzzle.textContent = game.puzzle; //getPuzzle();
-  message.innerHTML = game.statusMessage; // showMessage();
+  puzzle.textContent = game.puzzle;
+  message.innerHTML = game.statusMessage;
+});
+
+getPuzzle((error, puzzle) => {
+  if (error) {
+    console.log(`Error: ${error}`);
+  } else {
+    console.log(puzzle);
+  }
+});
+
+getCountry("US", (error, country) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`Country name: ${country.name}`);
+  }
 });
