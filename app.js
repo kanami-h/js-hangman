82cd30a1e12f07cd51e1dd4c4410a0100ce44971
Kanami <kanami.h217@gmail.com>
2021-10-27 11:11:18 -0400

    add: fetch API and asynchronous functions

--- app.js
+++ app.js
@@ -1,35 +1,28 @@
-const game = new Hangman("car parts", 5);
-
-const puzzle = document.querySelector(".puzzle");
+const puzzleEl = document.querySelector(".puzzle");
 const message = document.querySelector(".status");
 const remainingNumber = document.querySelector(".remaining-num");
-
-// initial settings
-window.addEventListener("DOMContentLoaded", () => {
-  puzzle.textContent = game.puzzle;
-  message.innerHTML = game.statusMessage;
-});
+const resetBtn = document.getElementById("reset");
+let game;
 
 window.addEventListener("keypress", (e) => {
   const guess = String.fromCharCode(e.charCode);
   game.makeGuess(guess);
-  // display the puzzle
-  puzzle.textContent = game.puzzle;
+  render();
+});
+
+const render = () => {
+  puzzleEl.textContent = game.puzzle;
   message.innerHTML = game.statusMessage;
+};
+
+const startGame = async () => {
+  const puzzle = await getPuzzle("2");
+  game = new Hangman(puzzle, 9);
+  render();
+};
+
+resetBtn.addEventListener("click", () => {
+  startGame();
 });
 
-getPuzzle((error, puzzle) => {
-  if (error) {
-    console.log(`Error: ${error}`);
-  } else {
-    console.log(puzzle);
-  }
-});
-
-getCountry("US", (error, country) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(`Country name: ${country.name}`);
-  }
-});
+startGame();
--- requests.js
+++ requests.js
@@ -1,41 +1,11 @@
-const getPuzzle = (callback) => {
-  const request = new XMLHttpRequest();
-  request.addEventListener("readystatechange", (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-      const data = JSON.parse(e.target.responseText);
-      callback(undefined, data.puzzle);
-    } else if (e.target.readyState === 4) {
-      callback("An error has taken place", undefined);
-    }
-  });
-
-  request.open("GET", "http://puzzle.mead.io/puzzle?wordCount=3");
-  request.send();
-};
-
-// API = f4e449b4f8a660ebac8fd45056d3329e
-
-const getCountry = (countryCode, callback) => {
-  const countryRequest = new XMLHttpRequest();
-  const countryCode = "CA";
-
-  countryRequest.addEventListener("readystatechange", (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-      const countryData = JSON.parse(e.target.responseText);
-      countryData.map((country) => {
-        if (cpintry.alpha2Code === countryCode) {
-          console.log(country.name);
-        }
-      });
-      callback(undefined, country);
-    } else if (e.target.readyState === 4) {
-      callback("Unable to fetch data");
-    }
-  });
-
-  countryRequest.open(
-    "GET",
-    "http://api.countrylayer.com/v2/all?access_key=f4e449b4f8a660ebac8fd45056d3329e",
+const getPuzzle = async (wordCount) => {
+  const response = await fetch(
+    `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`,
   );
-  countryRequest.send();
+  if (response.status === 200) {
+    const data = await response.json();
+    return data.puzzle;
+  } else {
+    throw new Error("Unable to get puzzle");
+  }
 };
